f6c71c8f76801c58c53d0fe989b6443c0e210f2e
Voronchikhin <m.voronchikhin@g.nsu.ru>
2019-01-18 19:54:33 +0700

    change model layer

--- app/src/main/java/com/example/neofr/habitcounter/view/HabitActivity.kt
+++ app/src/main/java/com/example/neofr/habitcounter/view/HabitActivity.kt
@@ -1,10 +1,17 @@
 package com.example.neofr.habitcounter.view
 
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import com.example.neofr.habitcounter.R
 import com.example.neofr.habitcounter.view.base.SingleFragmentActivity
 
 class HabitActivity : SingleFragmentActivity() {
 
     override fun createFragment(): Fragment = HabitFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+    }
 }
