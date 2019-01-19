c47fb99ecb18cf6b6df27b9784dcd689e42f3f0b
Voronchikhin <m.voronchikhin@g.nsu.ru>
2019-01-19 16:10:45 +0700

    add missed src

---
+++ app/src/main/java/com/example/neofr/habitcounter/model/HabitDataSource.kt
@@ -0,0 +1,5 @@
+package com.example.neofr.habitcounter.model
+
+interface HabitDataSource {
+
+}
---
+++ app/src/main/java/com/example/neofr/habitcounter/model/HabitDataSourceImpl.kt
@@ -0,0 +1,3 @@
+package com.example.neofr.habitcounter.model
+
+class HabitDataSourceImpl : HabitDataSource
