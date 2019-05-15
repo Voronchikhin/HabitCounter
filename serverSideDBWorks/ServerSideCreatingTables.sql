fc60c81c76d761bf98f12066d7b5e00363b5e4f4
rbdv <i.budaeva@g.nsu.ru>
2019-05-16 00:15:33 +0700

    +table, trigger, procedures

--- serverSideDBWorks/ServerSideCreatingTables.sql
+++ serverSideDBWorks/ServerSideCreatingTables.sql
@@ -48,3 +48,84 @@ CREATE TABLE ServerLog (
   FOREIGN KEY (habit_id) REFERENCES Habit(habit_id)
 )
 
+CREATE TABLE MonthServerLog (
+  log_record_id INT IDENTITY(1,1) PRIMARY KEY,
+  habit_id INT,
+  user_id INT,
+  actionDate DATE
+    -- other fields
+  FOREIGN KEY (user_id) REFERENCES AppUser(user_id),
+  FOREIGN KEY (habit_id) REFERENCES Habit(habit_id)
+)
+
+CREATE OR ALTER TRIGGER new_habit_check ON Habit
+  INSTEAD OF INSERT, UPDATE
+AS
+  BEGIN
+    IF exists(SELECT * from Habit where Habit.habit_name = (select habit_name from inserted))
+      update Habit set habit_description = Habit.habit_description + ' ' +
+                                           ( select habit_description from inserted)
+      where Habit.habit_name = (select habit_name from inserted);
+    ELSE
+      INSERT INTO Habit (habit_name, habit_description)
+        select habit_name, habit_description from inserted;
+  END;
+
+CREATE OR ALTER PROCEDURE addLogRecord(@habit_id INT, @user_id INT, @actionDate DATE)
+AS
+  BEGIN
+    SET NOCOUNT ON;
+    INSERT INTO MonthServerLog VALUES (@habit_id, @user_id, @actionDate);
+  END;
+GO;
+
+
+
+CREATE OR ALTER PROCEDURE clearMonthServerLog
+AS
+  BEGIN
+    SET NOCOUNT ON;
+    TRUNCATE TABLE MonthServerLog;
+    --if trigger on delete added:
+    --DELETE MonthServerLog;
+  END;
+GO;
+
+CREATE OR ALTER PROC countOfActions (@habit_id INT, @user_id INT)
+AS
+  BEGIN
+    DECLARE @count INT;
+    SELECT @count = count(log_record_id) FROM MonthServerLog WHERE
+      user_id = @user_id AND habit_id = @habit_id
+    SET @count = ISNULL(@count, 0);
+    RETURN @count;
+  END;
+GO;
+
+CREATE OR ALTER PROCEDURE updateStatistics
+AS
+  BEGIN
+    DECLARE @CURSOR CURSOR
+    DECLARE @habit_id INT
+    DECLARE @user_id INT
+    SET @CURSOR  = CURSOR SCROLL
+    FOR
+    SELECT habit_id, user_id FROM MonthServerLog;
+    OPEN @CURSOR
+    FETCH NEXT FROM @CURSOR INTO @habit_id, @user_id;
+    WHILE @@FETCH_STATUS = 0
+      BEGIN
+        IF EXISTS(SELECT * FROM ServerLog WHERE habit_id=@habit_id AND user_id=@user_id)
+          BEGIN
+            UPDATE ServerLog SET ServerLog.counter = ServerLog.counter + 1
+            WHERE habit_id=@habit_id AND user_id=@user_id
+          END
+        ELSE
+          INSERT INTO ServerLog VALUES (@user_id, @habit_id, GETDATE(), 1);
+        FETCH NEXT FROM @CURSOR INTO @habit_id, @user_id;
+      END
+    CLOSE @CURSOR
+    EXECUTE clearMonthServerLog;
+  END
+  GO;
+
